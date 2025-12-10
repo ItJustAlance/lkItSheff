e7d990ad00fc802554ca7141ea24be285e1d02bb
ratamanov <r399055750@gmail.com>
2025-12-10 14:18:10 +0500

    меню мобильной версии

--- src/js/static-js.js
+++ src/js/static-js.js
@@ -2,7 +2,17 @@ import $ from "jquery";
 
 $(function() {
   console.log($);
-  
+	
+	$(".js-menu-open").on("click", function (){
+		$(this).toggleClass("--menu-open");
+		$(".mobile-menu__layout").toggleClass("--show")
+	});
+	
+	$(".js-mobile-menu__item-title").on("click", function (){
+		$(this).parent().toggleClass("--active");
+	});
+	
+	
   $(".js-btn-modal").on("click", function (){
     const modal = $(this).data("modal");
     $("body").addClass("lock just-modal--default")
